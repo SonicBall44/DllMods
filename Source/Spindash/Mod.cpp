b672408b1e04a97f671c8f4e3659bd9ea5ef5101
SonicBall44 <98436123+SonicBall44@users.noreply.github.com>
2023-03-13 21:50:51 -0600

    Removed the WIP message

--- Source/Spindash/Mod.cpp
+++ Source/Spindash/Mod.cpp
@@ -10,17 +10,13 @@ extern "C" _declspec(dllexport) void Init()
 	Player::Install();
 	INIReader reader("Spindash.ini");
 	Player::cSonic_spindashSpeed = reader.GetInteger("Speed", "SpeedMax", 110);
-	if (Player::cSonic_spindashSpeed < 10 )
-	{
-		MessageBox(nullptr, TEXT("The mininum Spindash speed should be 15, setting to default speed"), TEXT("Spindash Speed"), MB_ICONERROR);
-		Player::cSonic_spindashSpeed = 110;
 
-	}
+
 }
 
 extern "C" __declspec(dllexport) void PostInit() {
 
-		MessageBox(nullptr, TEXT("This mod is in work in progress"), TEXT("Modren Sonic Spindash"), MB_ICONERROR);
+		
 
 
 	}
